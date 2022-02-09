a8d74651539e11d386b26afccb934f58f096292f
BuildTools <unconfigured@null.spigotmc.org>
2022-02-09 11:44:06 +0400

    added comments and cleaned up code

--- main.py
+++ main.py
@@ -2,18 +2,33 @@ import discord
 import requests
 import json
 
+# If you want to run this bot yourself, you can either make a token.json
+# file and make a value called "token",
+# Then leave this code and it should work.
+# --------
+# If you arent in danger of exposing your token,
+# you can just replace those 3 token lines below with
+# --------
+# token = "Your token here"
+# --------
+
+
+# Importing token from token.json file. I did this so you cant see the bot token im using.
 tokenfile = open('token.json')
-
 tokenjson = json.load(tokenfile)
-
 token = tokenjson['token']
 
 client = discord.Client()
 
 
+# On bot startup
 @client.event
 async def on_ready():
+    # Changes the bot presence to "Listening to WTB Users👍"
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="WTB Users👍"))
     print(f'{client.user} has connected to Discord!')
+
+# On message sent in discord
 
 
 @client.event
@@ -21,14 +36,19 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith("w2b lookup"):
+    # If the message content startswith "wtb lookup"
+    if message.content.startswith("wtb lookup"):
+
+        # Splits the message into "wtb lookup " and the UserId the user inputted
         rawmsg = message.content
         splitted = rawmsg.split("lookup ")
         userid = splitted[1]
 
+        # Sends a request to the WTB API for a users profile data
         r = requests.get(
             'https://worldtobuild.com/api/user/FetchProfilePreview?UserID=' + userid).json()
 
+        # If the request was invalid
         if r["Success"] == False:
             embed = discord.Embed(title="Lookup", color=0xf40b0b)
             embed.set_author(name="World To Build Bot")
@@ -36,6 +56,7 @@ async def on_message(message):
                 name="❌Error", value="User not found.", inline=False)
             await message.reply(embed=embed)
 
+        # If the request was successful
         elif r["Success"] == True:
 
             # Embed settings
