507ede00bf5a084bbf1bc411f478499a66430448
BuildTools <unconfigured@null.spigotmc.org>
2022-02-13 16:10:54 +0400

    v1.0.3

--- main.py
+++ main.py
@@ -163,6 +163,33 @@ async def ping(ctx):
     embed.add_field(name="Ping", value=ping, inline=False)
     await ctx.reply(embed=embed)
 
+# serverinfo
+
+
+async def serverinfo(ctx):
+    name = str(ctx.guild.name)
+    description = str(ctx.guild.description)
+
+    owner = str(ctx.guild.owner)
+    id = str(ctx.guild.id)
+    region = str(ctx.guild.region)
+    memberCount = str(ctx.guild.member_count)
+
+    icon = str(ctx.guild.icon_url)
+
+    embed = discord.Embed(
+        title=name + " Server Information",
+        description=description,
+        color=discord.Color.orange()
+    )
+    embed.set_thumbnail(url=icon)
+    embed.add_field(name="Owner", value=owner, inline=True)
+    embed.add_field(name="Server ID", value=id, inline=True)
+    embed.add_field(name="Region", value=region, inline=True)
+    embed.add_field(name="Members", value=memberCount, inline=True)
+
+    await ctx.send(embed=embed)
+
 
 # Help command
 @bot.command(name="help")
